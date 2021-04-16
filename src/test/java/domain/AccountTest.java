7e0204ba583b42cf40f1ad4233fea1df539df2d5
CitizenFiftyTwo <darkdroguza@hotmail.fr>
2021-04-16 23:37:18 +0200

    [US-1] Parameterized tests in deposit money

--- src/test/java/domain/AccountTest.java
+++ src/test/java/domain/AccountTest.java
@@ -1,19 +1,25 @@
 package domain;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class AccountTest {
     
     Account account = new Account();
-    
-    @Test
-    public void depositMoney_should_add_money_to_balance() {
-        Amount depositAmount = Amount.of(42);
 
-        account.deposit(depositAmount);
+    @DisplayName("Deposit money is OK")
+    @ParameterizedTest(name = "Deposit {0} money in account should result with {1} in balance")
+    @CsvSource({
+            "0, 0.00",
+            "42, 42.00",
+            "42.5, 42.50",
+    })
+    public void depositMoney_should_add_money_to_balance(double depositAmount, double balance) {
+        account.deposit(Amount.of(depositAmount));
 
-        assertThat(account.getBalance()).isEqualTo(depositAmount);
+        assertThat(account.getBalance()).isEqualTo(Amount.of(balance));
     }
 }
