c10ec515c61be309538a2f68a81e669f4aefa981
CitizenFiftyTwo <darkdroguza@hotmail.fr>
2021-04-16 23:43:44 +0200

    [US-1] Test in deposit money accumulate two deposits and round up to 2 decimals

--- src/test/java/domain/AccountTest.java
+++ src/test/java/domain/AccountTest.java
@@ -11,14 +11,22 @@ class AccountTest {
     Account account = new Account();
 
     @DisplayName("Deposit money is OK")
-    @ParameterizedTest(name = "Deposit {0} money in account should result with {1} in balance")
+    @ParameterizedTest(name = "Deposit {0} and {1} in account should result with {2} in balance")
     @CsvSource({
-            "0, 0.00",
-            "42, 42.00",
-            "42.5, 42.50",
+            "0, 0, 0.00",
+            "0, 42, 42.00",
+            "0, 42.5, 42.50",
+            "42, 8, 50.00",
+            "42.5, 7.5, 50.00",
+            "42.55, 7.45, 50.00",
+            "42.554, 7.454, 50.00",
+            "42.555, 7.455, 50.02",
     })
-    public void depositMoney_should_add_money_to_balance(double depositAmount, double balance) {
-        account.deposit(Amount.of(depositAmount));
+    public void depositMoney_should_add_money_to_balance(double firstDepositAmount,
+                                                         double secondDepositAmount,
+                                                         double balance) {
+        account.deposit(Amount.of(firstDepositAmount));
+        account.deposit(Amount.of(secondDepositAmount));
 
         assertThat(account.getBalance()).isEqualTo(Amount.of(balance));
     }
