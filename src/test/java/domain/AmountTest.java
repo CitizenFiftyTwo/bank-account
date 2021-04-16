b4ad310bfed031ec7327fb3b000d880b2a9f9aca
CitizenFiftyTwo <darkdroguza@hotmail.fr>
2021-04-16 21:19:08 +0200

    [US-1] Parameterized test in Amount

--- src/test/java/domain/AmountTest.java
+++ src/test/java/domain/AmountTest.java
@@ -1,18 +1,25 @@
 package domain;
 
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.math.BigDecimal;
 
 class AmountTest {
 
-    @Test
-    public void amountOf_expectedValue_should_be_equal_to_instanciated_amount_with_same_value() {
-        double value = 52;
+    @DisplayName("Amount instanciation is OK")
+    @ParameterizedTest(name = "Amount instanciation with value {0} is instanciated with value {1}")
+    @CsvSource({
+            "0, 0.0",
+            "42, 42.0",
+            "42.5, 42.5",
+            "42.52, 42.52"
+    })
+    public void amountOf_expectedValue_should_be_equal_to_instanciated_amount_with_same_value(double instanceValue, BigDecimal expectedValue) {
+        Amount expectedAmount = new Amount(expectedValue);
 
-        Amount expectedAmount = new Amount(BigDecimal.valueOf(value));
-
-        Assertions.assertThat(expectedAmount).isEqualTo(Amount.of(value));
+        Assertions.assertThat(expectedAmount).isEqualTo(Amount.of(instanceValue));
     }
 }
