e4c6d93d58a3041089568f50867ba19a9fd809ee
CitizenFiftyTwo <darkdroguza@hotmail.fr>
2021-04-17 00:07:35 +0200

    [US-2] depositMoney tests now use the account instanciation

--- src/test/java/domain/AccountTest.java
+++ src/test/java/domain/AccountTest.java
@@ -8,8 +8,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class AccountTest {
 
-    Account account = new Account();
-
     @DisplayName("Instanciate account with a specified amount is OK")
     @ParameterizedTest(name = "Instanciate an account with {0} in balance should result with {1} in balance")
     @CsvSource({
@@ -28,7 +26,7 @@ class AccountTest {
     }
 
     @DisplayName("Deposit money is OK")
-    @ParameterizedTest(name = "Deposit {0} and {1} in account should result with {2} in balance")
+    @ParameterizedTest(name = "Deposit {1} in account with {0} should result with {2} in balance")
     @CsvSource({
             "0, 0, 0.00",
             "0, 42, 42.00",
@@ -39,12 +37,13 @@ class AccountTest {
             "42.554, 7.454, 50.00",
             "42.555, 7.455, 50.02",
     })
-    public void depositMoney_should_add_money_to_balance(double firstDepositAmount,
-                                                         double secondDepositAmount,
-                                                         double balance) {
-        account.deposit(Amount.of(firstDepositAmount));
-        account.deposit(Amount.of(secondDepositAmount));
+    public void depositMoney_should_add_money_to_balance(double initialBalanceValue,
+                                                         double depositAmountValue,
+                                                         double expectedBalanceValue) {
+        Account account = Account.withBalance(Amount.of(initialBalanceValue));
 
-        assertThat(account.getBalance()).isEqualTo(Amount.of(balance));
+        account.deposit(Amount.of(depositAmountValue));
+
+        assertThat(account.getBalance()).isEqualTo(Amount.of(expectedBalanceValue));
     }
 }
